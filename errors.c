9e8093b35f496e0ebff9973ae36639f93a15e063
Banu-ie <i.banu.4757@gmail.com>
2026-02-21 15:39:13 +0000

    Task 4: handle PATH

--- errors.c
+++ errors.c
@@ -1,13 +1,7 @@
 #include "shell.h"
 #include <stdio.h>
-#include "shell.h"
 
-/**
- * print_not_found - prints command not found error
- * @st: shell state
- * @cmd: command name
- */
-void print_not_found(shell_state_t *st, char *cmd)
+void print_not_found(shell_state_t *st, const char *cmd)
 {
     fprintf(stderr, "%s: %d: %s: not found\n",
             st->prog, st->line_num, cmd);
